c92ae46c521e814dde64f5f071dd3f446de438b1
Owale128 <Christian.buaka@hotmail.com>
2025-03-08 21:14:37 +0100

    created state in the difficulty page

--- src/pages/Difficulty.tsx
+++ src/pages/Difficulty.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 
 const Difficulty = () => {
+  const [difficulty, setDifficulty] = useState('')
+
+  const handleDifficulty = (diffi: string) => {
+    setDifficulty(diffi)
+  }
   return (
     <div>
       <h1>Choose Difficulty</h1>
+      <button onClick={() => handleDifficulty('easy')}>Easy</button>
+      <button onClick={() => handleDifficulty('medium')}>Medium</button>
+      <button onClick={() => handleDifficulty('hard')}>Hard</button>
     </div>
   )
 }
