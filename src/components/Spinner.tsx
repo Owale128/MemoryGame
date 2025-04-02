f60996c0bfd3e73679bfc514e85ad63108ccc8a7
Owale128 <Christian.buaka@hotmail.com>
2025-04-02 17:51:46 +0200

    Added loading text in spinner component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,8 @@
 import { Rings } from "react-loader-spinner"
 const Spinner = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1 className="text-2xl">Loading..Please wait</h1>
       <Rings color='#3498db' height={350} width={350} />
     </div>
   )
